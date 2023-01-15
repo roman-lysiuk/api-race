1afcaba893b1d509df65e185abfa86dc8b4ce8ce
Roman Lysiuk <gekalo1025@gmail.com>
2023-01-15 15:03:38 +0200

    feat: add drawMenu method app

--- Api-Race/src/view/app/app.ts
+++ Api-Race/src/view/app/app.ts
@@ -7,7 +7,28 @@ class App {
     this.garage = new Garage();
   }
 
-  start() {}
+  drawMenu() {
+    const body: HTMLElement = document.body;
+    const menu = document.createElement('div');
+    const buttonGarage: HTMLButtonElement = document.createElement('button');
+    const buttonWinners: HTMLButtonElement = document.createElement('button');
+
+    menu.classList.add('menu');
+
+    buttonGarage.innerHTML = 'To Garage';
+    buttonWinners.innerHTML = 'To Winners';
+
+    buttonGarage.setAttribute('id', 'btn-garage');
+    buttonWinners.setAttribute('id', 'btn-winners');
+    menu.appendChild(buttonGarage);
+    menu.appendChild(buttonWinners);
+
+    body.append(menu);
+  }
+
+  start() {
+    this.drawMenu();
+  }
 }
 
 export default App;
